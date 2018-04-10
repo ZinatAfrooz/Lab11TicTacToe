fdd3b9e81ea8043d9e93c35826df9e577c2e2ac0
ZinatAfrooz <S01012776@acad.tri-c.edu>
2018-04-09 23:13:35 -0400

    Tic Tac Toe

---
+++ Lab11TicTacToe/Lab11TicTacToe/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTIcTac
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPlay1 = New System.Windows.Forms.Button()
+        Me.btnPlay2 = New System.Windows.Forms.Button()
+        Me.btnPlay3 = New System.Windows.Forms.Button()
+        Me.btnPlay4 = New System.Windows.Forms.Button()
+        Me.btnPlay5 = New System.Windows.Forms.Button()
+        Me.btnPlay6 = New System.Windows.Forms.Button()
+        Me.btnPlay7 = New System.Windows.Forms.Button()
+        Me.btnPlay8 = New System.Windows.Forms.Button()
+        Me.btnPlay9 = New System.Windows.Forms.Button()
+        Me.lblWin = New System.Windows.Forms.Label()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnPlay1
+        '
+        Me.btnPlay1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay1.Location = New System.Drawing.Point(37, 26)
+        Me.btnPlay1.Name = "btnPlay1"
+        Me.btnPlay1.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay1.TabIndex = 0
+        Me.btnPlay1.UseVisualStyleBackColor = True
+        '
+        'btnPlay2
+        '
+        Me.btnPlay2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay2.Location = New System.Drawing.Point(104, 26)
+        Me.btnPlay2.Name = "btnPlay2"
+        Me.btnPlay2.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay2.TabIndex = 1
+        Me.btnPlay2.UseVisualStyleBackColor = True
+        '
+        'btnPlay3
+        '
+        Me.btnPlay3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay3.Location = New System.Drawing.Point(175, 26)
+        Me.btnPlay3.Name = "btnPlay3"
+        Me.btnPlay3.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay3.TabIndex = 2
+        Me.btnPlay3.UseVisualStyleBackColor = True
+        '
+        'btnPlay4
+        '
+        Me.btnPlay4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay4.Location = New System.Drawing.Point(37, 75)
+        Me.btnPlay4.Name = "btnPlay4"
+        Me.btnPlay4.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay4.TabIndex = 3
+        Me.btnPlay4.UseVisualStyleBackColor = True
+        '
+        'btnPlay5
+        '
+        Me.btnPlay5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay5.Location = New System.Drawing.Point(104, 75)
+        Me.btnPlay5.Name = "btnPlay5"
+        Me.btnPlay5.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay5.TabIndex = 4
+        Me.btnPlay5.UseVisualStyleBackColor = True
+        '
+        'btnPlay6
+        '
+        Me.btnPlay6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay6.Location = New System.Drawing.Point(175, 75)
+        Me.btnPlay6.Name = "btnPlay6"
+        Me.btnPlay6.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay6.TabIndex = 5
+        Me.btnPlay6.UseVisualStyleBackColor = True
+        '
+        'btnPlay7
+        '
+        Me.btnPlay7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay7.Location = New System.Drawing.Point(37, 123)
+        Me.btnPlay7.Name = "btnPlay7"
+        Me.btnPlay7.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay7.TabIndex = 6
+        Me.btnPlay7.UseVisualStyleBackColor = True
+        '
+        'btnPlay8
+        '
+        Me.btnPlay8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay8.Location = New System.Drawing.Point(104, 123)
+        Me.btnPlay8.Name = "btnPlay8"
+        Me.btnPlay8.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay8.TabIndex = 7
+        Me.btnPlay8.UseVisualStyleBackColor = True
+        '
+        'btnPlay9
+        '
+        Me.btnPlay9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay9.Location = New System.Drawing.Point(175, 123)
+        Me.btnPlay9.Name = "btnPlay9"
+        Me.btnPlay9.Size = New System.Drawing.Size(46, 33)
+        Me.btnPlay9.TabIndex = 8
+        Me.btnPlay9.UseVisualStyleBackColor = True
+        '
+        'lblWin
+        '
+        Me.lblWin.AutoSize = True
+        Me.lblWin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblWin.Location = New System.Drawing.Point(37, 181)
+        Me.lblWin.Name = "lblWin"
+        Me.lblWin.Padding = New System.Windows.Forms.Padding(0, 0, 140, 0)
+        Me.lblWin.Size = New System.Drawing.Size(142, 15)
+        Me.lblWin.TabIndex = 9
+        '
+        'btnNewGame
+        '
+        Me.btnNewGame.Location = New System.Drawing.Point(37, 213)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(75, 23)
+        Me.btnNewGame.TabIndex = 10
+        Me.btnNewGame.Text = "New Game"
+        Me.btnNewGame.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(142, 212)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmTIcTac
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(275, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.lblWin)
+        Me.Controls.Add(Me.btnPlay9)
+        Me.Controls.Add(Me.btnPlay8)
+        Me.Controls.Add(Me.btnPlay7)
+        Me.Controls.Add(Me.btnPlay6)
+        Me.Controls.Add(Me.btnPlay5)
+        Me.Controls.Add(Me.btnPlay4)
+        Me.Controls.Add(Me.btnPlay3)
+        Me.Controls.Add(Me.btnPlay2)
+        Me.Controls.Add(Me.btnPlay1)
+        Me.Name = "frmTIcTac"
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPlay1 As Button
+    Friend WithEvents btnPlay2 As Button
+    Friend WithEvents btnPlay3 As Button
+    Friend WithEvents btnPlay4 As Button
+    Friend WithEvents btnPlay5 As Button
+    Friend WithEvents btnPlay6 As Button
+    Friend WithEvents btnPlay7 As Button
+    Friend WithEvents btnPlay8 As Button
+    Friend WithEvents btnPlay9 As Button
+    Friend WithEvents lblWin As Label
+    Friend WithEvents btnNewGame As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Lab11TicTacToe/Lab11TicTacToe/Form1.vb
@@ -0,0 +1,162 @@
+﻿Public Class frmTIcTac
+    'Private intRand As Integer = 1
+    Private intSet As Integer = 0
+
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
+        btnPlay1.Text = ""
+        btnPlay1.Enabled = True
+        btnPlay2.Text = ""
+        btnPlay2.Enabled = True
+        btnPlay3.Text = ""
+        btnPlay3.Enabled = True
+        btnPlay4.Text = ""
+        btnPlay4.Enabled = True
+        btnPlay5.Text = ""
+        btnPlay5.Enabled = True
+        btnPlay6.Text = ""
+        btnPlay6.Enabled = True
+        btnPlay7.Text = ""
+        btnPlay7.Enabled = True
+        btnPlay8.Text = ""
+        btnPlay8.Enabled = True
+        btnPlay9.Text = ""
+        btnPlay9.Enabled = True
+        lblWin.Text = ""
+
+        intSet = 0
+    End Sub
+
+    Private Sub frmTIcTac_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnPlay1.Text = ""
+        btnPlay1.Enabled = True
+        btnPlay2.Text = ""
+        btnPlay2.Enabled = True
+        btnPlay3.Text = ""
+        btnPlay3.Enabled = True
+        btnPlay4.Text = ""
+        btnPlay4.Enabled = True
+        btnPlay5.Text = ""
+        btnPlay5.Enabled = True
+        btnPlay6.Text = ""
+        btnPlay6.Enabled = True
+        btnPlay7.Text = ""
+        btnPlay7.Enabled = True
+        btnPlay8.Text = ""
+        btnPlay8.Enabled = True
+        btnPlay9.Text = ""
+        btnPlay9.Enabled = True
+
+        intSet = 0
+    End Sub
+
+    Sub ComputerPlay()
+        Dim objRandon As New Random
+        Dim intRandom As Integer
+
+        intRandom = Integer.Parse(objRandon.Next(1, 10))
+
+        If intRandom = 1 And btnPlay1.Enabled Then
+            btnPlay1.Text = "O"
+            btnPlay1.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 2 And btnPlay2.Enabled Then
+            btnPlay2.Text = "O"
+            btnPlay2.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 3 And btnPlay3.Enabled Then
+            btnPlay3.Text = "O"
+            btnPlay3.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 4 And btnPlay4.Enabled Then
+            btnPlay4.Text = "O"
+            btnPlay4.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 5 And btnPlay5.Enabled Then
+            btnPlay5.Text = "O"
+            btnPlay5.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 6 And btnPlay6.Enabled Then
+            btnPlay6.Text = "O"
+            btnPlay6.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 7 And btnPlay7.Enabled Then
+            btnPlay7.Text = "O"
+            btnPlay7.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 8 And btnPlay8.Enabled Then
+            btnPlay8.Text = "O"
+            btnPlay8.Enabled = False
+            Exit Sub
+        End If
+
+        If intRandom = 9 And btnPlay9.Enabled Then
+            btnPlay9.Text = "O"
+            btnPlay9.Enabled = False
+            Exit Sub
+        End If
+
+        'ComputerPlay()
+
+    End Sub
+
+    Private Sub ClickButtons(sender As Object, e As EventArgs) Handles btnPlay1.Click, btnPlay2.Click, btnPlay3.Click, btnPlay4.Click, btnPlay5.Click, btnPlay6.Click, btnPlay7.Click, btnPlay8.Click, btnPlay8.Click, btnPlay9.Click
+
+        'You play
+        sender.text = "X"
+        sender.enabled = False
+        intSet += 1
+        CheckWinner()
+
+        'Computer plays
+        ComputerPlay()
+        intSet += 1
+        CheckWinner()
+
+    End Sub
+
+    Sub CheckWinner()
+        If intSet >= 9 Then
+            lblWin.Text = "Nobody is winner"
+        ElseIf btnPlay1.Text = "O" And btnPlay2.Text = "O" And btnPlay3.Text = "O" _
+            Or btnPlay4.Text = "O" And btnPlay5.Text = "O" And btnPlay6.Text = "O" _
+            Or btnPlay7.Text = "O" And btnPlay8.Text = "O" And btnPlay9.Text = "O" _
+            Or btnPlay1.Text = "O" And btnPlay4.Text = "O" And btnPlay7.Text = "O" _
+            Or btnPlay2.Text = "O" And btnPlay5.Text = "O" And btnPlay8.Text = "O" _
+            Or btnPlay3.Text = "O" And btnPlay6.Text = "O" And btnPlay9.Text = "O" _
+            Or btnPlay1.Text = "O" And btnPlay5.Text = "O" And btnPlay9.Text = "O" _
+            Or btnPlay1.Text = "O" And btnPlay2.Text = "O" And btnPlay3.Text = "O" _
+            Or btnPlay7.Text = "O" And btnPlay5.Text = "O" And btnPlay3.Text = "O" Then
+
+            lblWin.Text = "Computer winner!!"
+        ElseIf btnPlay1.Text = "X" And btnPlay2.Text = "X" And btnPlay3.Text = "X" _
+            Or btnPlay4.Text = "X" And btnPlay5.Text = "X" And btnPlay6.Text = "X" _
+            Or btnPlay7.Text = "X" And btnPlay8.Text = "X" And btnPlay9.Text = "X" _
+            Or btnPlay1.Text = "X" And btnPlay4.Text = "X" And btnPlay7.Text = "X" _
+            Or btnPlay2.Text = "X" And btnPlay5.Text = "X" And btnPlay8.Text = "X" _
+            Or btnPlay3.Text = "X" And btnPlay6.Text = "X" And btnPlay9.Text = "X" _
+            Or btnPlay1.Text = "X" And btnPlay5.Text = "X" And btnPlay9.Text = "X" _
+            Or btnPlay1.Text = "X" And btnPlay2.Text = "X" And btnPlay3.Text = "X" _
+            Or btnPlay7.Text = "X" And btnPlay5.Text = "X" And btnPlay3.Text = "X" Then
+
+            lblWin.Text = "You winner!!"
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
